fdd8b873eef327790d25747ecf0a9cbde6853a2a
WrathChaos <wrathchaos@hotmail.com>
2017-07-10 13:05:48 +0300

    Example is added.

--- Example/StompClientLib/ViewController.swift
+++ Example/StompClientLib/ViewController.swift
@@ -7,17 +7,62 @@
 //
 
 import UIKit
+import StompClientLib
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, StompClientLibDelegate {
+    
+    var socketClient = StompClientLib()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Connection with socket
+        registerSocket()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func registerSocket(){
+        let baseURL = "http://your-url-is-here/"
+        // Cut the first 7 character which are "http://" Not necessary!!!
+        let wsURL = baseURL.substring(from:baseURL.index(baseURL.startIndex, offsetBy: 7))
+        let completedWSURL = "ws://\(wsURL)gateway/websocket"
+        print("Completed WS URL : \(completedWSURL)")
+        let url = NSURL(string: completedWSURL)!
+        
+        socketClient.openSocketWithURLRequest(request: NSURLRequest(url: url as URL) , delegate: self as StompClientLibDelegate)
+    }
+    
+    func stompClientDidConnect(client: StompClientLib!) {
+        let topic = "/topic/your topic is here/"
+        print("Socket is Connected : \(topic)")
+        socketClient.subscribe(destination: topic)
+    }
+    
+    func stompClientDidDisconnect(client: StompClientLib!) {
+        print("Socket is Disconnected")
+    }
+    
+    func stompClientWillDisconnect(client: StompClientLib!, withError error: NSError) {
+        
+    }
+    
+    
+    func stompClient(client: StompClientLib!, didReceiveMessageWithJSONBody jsonBody: AnyObject?, withHeader header: [String : String]?, withDestination destination: String) {
+        print("DESTIONATION : \(destination)")
+        print("JSON BODY : \(String(describing: jsonBody))")
+    }
+    
+    
+    func serverDidSendReceipt(client: StompClientLib!, withReceiptId receiptId: String) {
+        print("Receipt : \(receiptId)")
+    }
+    
+    
+    func serverDidSendError(client: StompClientLib!, withErrorMessage description: String, detailedErrorMessage message: String?) {
+        print("Error : \(String(describing: message))")
+    }
+    
+    func serverDidSendPing() {
+        print("Server Ping")
     }
 
 }
